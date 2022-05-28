d3e583015b5a2a1f655a6a28f5ee337ee86c0ad1
Plut0r <ogunolaololade@gmail.com>
2022-05-28 14:41:22 +0100

    Track

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -2,12 +2,18 @@ import React from 'react';
 
 import './TrackList.css';
 
+import Track from '../Track/Track';
+
 class TrackList extends React.Component {
     render() {
         return (
             <div className="TrackList">
-    {/* <Track /> */}
-</div>
+                {
+                    this.props.tracks.map(track => {
+                        return <Track track={track} key={track.id}/>
+                    })
+                }
+            </div>
         )
     }
 }
